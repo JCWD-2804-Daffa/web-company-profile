53f80e68e66de7e29b8725e7bb634c91afd6df86
daffnr <daffanr20@gmail.com>
2024-09-12 03:19:15 +0700

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,149 +1,169 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import axios from "axios";
 
 const home = () => {
-  const [data, setData] = useState<any>([])
-  const [dataMembership, setDataMembership] = useState<any>([])
-  const [dataTestimony, setDataTestimony] = useState<any>([])
-  
-  const fetchData = async() => {
-    const headers = {
-      'Authorization': 'Bearer flPnjtBrWPNIWfl5Fs_m_D8QBup1SUWPQhpaYoDfUHI'
-    }
-    const url = `https://cdn.contentful.com/spaces/6xzpw1n4lyqh/environments/master/entries?content_type=homeContent`
+  const [data, setData] = useState<any>([]);
+  const [dataMembership, setDataMembership] = useState<any>([]);
+  const [dataTestimony, setDataTestimony] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    await axios.get(url, {headers})
-     .then((response) => {
-        setData(response.data.items)
+  const fetchData = async () => {
+    const headers = {
+      Authorization: "Bearer flPnjtBrWPNIWfl5Fs_m_D8QBup1SUWPQhpaYoDfUHI",
+    };
+    const url = `https://cdn.contentful.com/spaces/6xzpw1n4lyqh/environments/master/entries?content_type=homeContent`;
+
+    await axios
+      .get(url, { headers })
+      .then((response) => {
+        setData(response.data.items);
+        setLoading(false)
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+        setLoading(false)
+      });
+  };
 
-  const fetchDataMembership = async() => {
+  const fetchDataMembership = async () => {
     const headers = {
-      'Authorization': 'Bearer flPnjtBrWPNIWfl5Fs_m_D8QBup1SUWPQhpaYoDfUHI'
-    }
-    const url = `https://cdn.contentful.com/spaces/6xzpw1n4lyqh/environments/master/entries?content_type=membershipContent`
+      Authorization: "Bearer flPnjtBrWPNIWfl5Fs_m_D8QBup1SUWPQhpaYoDfUHI",
+    };
+    const url = `https://cdn.contentful.com/spaces/6xzpw1n4lyqh/environments/master/entries?content_type=membershipContent`;
 
-    await axios.get(url, {headers})
-     .then((response) => {
-        setDataMembership(response.data.items)
+    await axios
+      .get(url, { headers })
+      .then((response) => {
+        setDataMembership(response.data.items);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
-  const fetchDataTestimony = async() => {
+  const fetchDataTestimony = async () => {
     const headers = {
-      'Authorization': 'Bearer flPnjtBrWPNIWfl5Fs_m_D8QBup1SUWPQhpaYoDfUHI'
-    }
-    const url = `https://cdn.contentful.com/spaces/6xzpw1n4lyqh/environments/master/entries?content_type=testimonyContent`
+      Authorization: "Bearer flPnjtBrWPNIWfl5Fs_m_D8QBup1SUWPQhpaYoDfUHI",
+    };
+    const url = `https://cdn.contentful.com/spaces/6xzpw1n4lyqh/environments/master/entries?content_type=testimonyContent`;
 
-    await axios.get(url, {headers})
-     .then((response) => {
-        setDataTestimony(response.data.items)
+    await axios
+      .get(url, { headers })
+      .then((response) => {
+        setDataTestimony(response.data.items);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
-      fetchData()
-      fetchDataMembership()
-      fetchDataTestimony()
-    },[])
+    fetchData();
+    fetchDataMembership();
+    fetchDataTestimony();
+  }, []);
 
-    console.log(data)
-    console.log(dataMembership)
-    console.log(dataTestimony)
+  console.log(data);
+  console.log(dataMembership);
+  console.log(dataTestimony);
+
+  if (loading){
+    return (
+      <h1 className='text-2xl font-bold text-center'>Loading...</h1>
+    )
+  }
 
   return (
-    <div className='flex flex-col items-center justify-center gap-32'>
-      <section className='sm:flex items-center'>
+    <div className="flex flex-col items-center justify-center gap-32">
+      <section className="sm:flex items-center">
         <div>
-          <h1 className='xs:text-4xl sm:text-8xl font-bold'>{data[1]?.fields?.title}</h1>
-          <div className='flex gap-3 mt-10'>
+          <h1 className="xs:text-4xl sm:text-8xl font-bold">
+            {data[0]?.fields?.title}
+          </h1>
+          <div className="flex gap-3 mt-10">
             <Link href="/services">
-              <button className='bg-black text-white p-3 rounded-lg cursor-pointer'>Our Service</button>
+              <button className="bg-black text-white p-3 rounded-lg cursor-pointer">
+                Our Service
+              </button>
             </Link>
             <Link href="/about">
-              <button className='bg-black text-white p-3 rounded-lg cursor-pointer'>About Us</button>
+              <button className="bg-black text-white p-3 rounded-lg cursor-pointer">
+                About Us
+              </button>
             </Link>
           </div>
         </div>
-        <img className='xs:w-full xs:mt-10 sm:w-[50%] rounded' src={data[1]?.fields?.urlMedia} alt='image-banner' />
+        <img
+          className="xs:w-full xs:mt-10 sm:w-[50%] rounded"
+          src={data[0]?.fields?.urlMedia}
+          alt="image-banner"
+        />
       </section>
 
-      <section className='sm:flex items-center gap-[450px]'>
-        <div className='sm:w-48 xs:mb-10 sm:mb-0'>
-          <iframe className='xs:w-[340px] xs:h-[200px] sm:w-[560px] sm:h-[315px]' src={data[0]?.fields?.urlMedia} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+      <section className="sm:flex items-center gap-[450px]">
+        <div className="sm:w-48 xs:mb-10 sm:mb-0">
+          <iframe
+            className="xs:w-[340px] xs:h-[200px] sm:w-[560px] sm:h-[315px]"
+            src={data[1]?.fields?.urlMedia}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
         </div>
         <div>
-          <h1 className='text-4xl font-bold mb-3'>{data[0]?.fields?.title}</h1>
-          <p>
-            {data[0]?.fields?.descriptions}
-          </p>
+          <h1 className="text-4xl font-bold mb-3">{data[1]?.fields?.title}</h1>
+          <p>{data[1]?.fields?.descriptions}</p>
         </div>
       </section>
 
-      <section className='flex flex-col items-center'>
-        <h1 className='text-4xl font-bold mb-10'>
-          Membership
-        </h1>
-        <div className='flex xs:flex-col sm:flex-row gap-10'>
-          <div className='bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5'>
-            <h3 className='text-xl'>{dataMembership[2]?.fields?.monthTitle}</h3>
-            <p className='text-sm'>{dataMembership[2]?.fields?.descriptions}</p>
-            <p className='text-xl'>{dataMembership[2]?.fields?.price}</p>
+      <section className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-10">Membership</h1>
+        <div className="flex xs:flex-col sm:flex-row gap-10">
+          <div className="bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5">
+            <h3 className="text-xl">{dataMembership[2]?.fields?.monthTitle}</h3>
+            <p className="text-sm">{dataMembership[2]?.fields?.descriptions}</p>
+            <p className="text-xl">{dataMembership[2]?.fields?.price}</p>
           </div>
-          <div className='bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5'>
-            <h3 className='text-xl'>{dataMembership[1]?.fields?.monthTitle}</h3>
-            <p className='text-sm'>{dataMembership[1]?.fields?.descriptions}</p>
-            <p className='text-xl'>{dataMembership[1]?.fields?.price}</p>
+          <div className="bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5">
+            <h3 className="text-xl">{dataMembership[1]?.fields?.monthTitle}</h3>
+            <p className="text-sm">{dataMembership[1]?.fields?.descriptions}</p>
+            <p className="text-xl">{dataMembership[1]?.fields?.price}</p>
           </div>
-          <div className='bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5'>
-            <h3 className='text-xl'>{dataMembership[0]?.fields?.monthTitle}</h3>
-            <p className='text-sm'>{dataMembership[0]?.fields?.descriptions}</p>
-            <p className='text-xl'>{dataMembership[0]?.fields?.price}</p>
+          <div className="bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5">
+            <h3 className="text-xl">{dataMembership[0]?.fields?.monthTitle}</h3>
+            <p className="text-sm">{dataMembership[0]?.fields?.descriptions}</p>
+            <p className="text-xl">{dataMembership[0]?.fields?.price}</p>
           </div>
         </div>
       </section>
 
-      <section className='flex flex-col items-center'>
-        <h1 className='text-4xl font-bold mb-10'>
-          Our Testimony
-        </h1>
-        <div className='flex xs:flex-col sm:flex-row gap-10'>
-          <div className='bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5'>
-            <h3 className='text-xl'>{dataTestimony[2]?.fields?.name}</h3>
-            <p className='text-sm italic'>
-            {dataTestimony[2]?.fields?.descriptions}
+      <section className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-10">Our Testimony</h1>
+        <div className="flex xs:flex-col sm:flex-row gap-10">
+          <div className="bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5">
+            <h3 className="text-xl">{dataTestimony[2]?.fields?.name}</h3>
+            <p className="text-sm italic">
+              {dataTestimony[2]?.fields?.descriptions}
             </p>
           </div>
-          <div className='bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5'>
-            <h3 className='text-xl'>{dataTestimony[1]?.fields?.name}</h3>
-            <p className='text-sm italic'>
-            {dataTestimony[1]?.fields?.descriptions}
+          <div className="bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5">
+            <h3 className="text-xl">{dataTestimony[1]?.fields?.name}</h3>
+            <p className="text-sm italic">
+              {dataTestimony[1]?.fields?.descriptions}
             </p>
           </div>
-          <div className='bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5'>
-            <h3 className='text-xl'>{dataTestimony[0]?.fields?.name}</h3>
-            <p className='text-sm italic'>
-            {dataTestimony[0]?.fields?.descriptions}
+          <div className="bg-black text-white text-center p-10 rounded-lg flex flex-col gap-5">
+            <h3 className="text-xl">{dataTestimony[0]?.fields?.name}</h3>
+            <p className="text-sm italic">
+              {dataTestimony[0]?.fields?.descriptions}
             </p>
           </div>
         </div>
       </section>
     </div>
-    
-  )
-}
+  );
+};
 
-export default home
+export default home;
